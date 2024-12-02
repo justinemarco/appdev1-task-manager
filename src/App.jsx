6612a8e001252b2559c64b464366190e958bdb84
justinemarco <justinejynnepatricemarco@student.laverdad.edu.ph>
2024-12-02 21:53:46 +0800

    Done creating component for delete task function and add task function

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import { useEffect } from 'react';
 import './App.css'
 import { db } from './firebase'
-import { collection, doc, getDocs, deleteDoc, updateDoc, getDoc } from 'firebase/firestore'
+import { collection, doc, getDocs, deleteDoc, addDoc, updateDoc, getDoc } from 'firebase/firestore'
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
   //Fetch docs from Firestore
   const fetchTasks = async () => {
@@ -29,8 +32,31 @@ const deleteTask = async (id) => {
   setTasks( (prevTasks) => prevTasks.filter(task => task.id !== id) )
 }
 
+// Adding Tasks Function
+  const addTask = async (e) => {
+    e.preventDefault();
+    const collectionRef = collection(db, 'tasks');
+    const docRef = await addDoc(collectionRef, {
+      title: title,
+      body: body,
+      status: 'pending'
+    })
+    setTitle('')
+    setBody('')
+    alert('Task Successfully Added!')
+  }
+
   return (
     <>
+
+      <div className='formStyle'>
+        <form onSubmit={addTask}>
+          <h3>Add a Task</h3>
+          <input type="text" name="title" id="title" placeholder="title" value={null} required onChange={(e) => setTitle(e.target.value)}/>
+          <textarea name="description" id="description" placeholder='task description' value={null} required onChange={(e) => setBody(e.target.value)}></textarea>
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
       {
         tasks.map((task) => (
           <div key={task.id}> 
